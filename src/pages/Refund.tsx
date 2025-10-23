bba194e6cd7459248365d4ecd6268c9c394a5066
samih freire <samihcisne@gmail.com>
2025-10-23 17:15:41 -0300

    feat: criado page de solicitação de reembolso

--- src/pages/Refund.tsx
+++ src/pages/Refund.tsx
@@ -1,14 +1,26 @@
-import { useState } from "react"
+import { use, useState } from "react"
 
 import { CATEGORIES, CATEGORIES_KEYS } from "../utils/categories"
 import { Input } from "../components/Input"
 import { Select } from "../components/Select"
+import { Upload } from "../components/Upload"
+import { Button } from "../components/Button"
 
 export function Refund() {
+    const [name, setName] = useState("");
+    const [amount, setAmount] = useState("");
     const [category, setCategory] = useState("");
+    const [isLoading, setIsLoading] = useState(false);
+    const [filename, setFilename] = useState<File | null>(null);
+
+    function onSubmit(e: React.FormEvent) {
+        e.preventDefault();
+
+        console.log(name, amount, category, filename);
+    }
 
     return (
-        <form className="bg-gray-500 w-full rounded-xl flex flex-col p-10 gap-6 lg:min-w-[512px]">
+        <form onSubmit={onSubmit} className="bg-gray-500 w-full rounded-xl flex flex-col p-10 gap-6 lg:min-w-[512px]">
             <header>
                 <h1 className="text-xl font-bold text-gray-100">
                     Solicitação de reembolso
@@ -19,16 +31,25 @@ export function Refund() {
                 </p>
             </header>
 
-            <Input required legend="Nome da solicitação" />
-            <Select required legend="Categoria" value={category} onChange={(e) => setCategory(e.target.value)}>
-                {
-                    // Percorremos a lista de categorias com suas chaves
-                    // Para cada chave de categoria buscamos na lista de categorias o seu nome
-                    CATEGORIES_KEYS.map((category) => (
-                        <option key={category} value={category}>{CATEGORIES[category].name}</option>
-                    ))
-                }
-            </Select>
+            <Input required legend="Nome da solicitação" value={name} onChange={(e) => setName(e.target.value)} />
+
+            <div className="flex gap-4">
+                <Select required legend="Categoria" value={category} onChange={(e) => setCategory(e.target.value)}>
+                    {
+                        // Percorremos a lista de categorias com suas chaves
+                        // Para cada chave de categoria buscamos na lista de categorias o seu nome
+                        CATEGORIES_KEYS.map((category) => (
+                            <option key={category} value={category}>{CATEGORIES[category].name}</option>
+                        ))
+                    }
+                </Select>
+
+                <Input legend="Valor" required value={amount} onChange={(e) => setAmount(e.target.value)} />
+            </div>
+
+            <Upload filename={filename && filename.name} onChange={(e) => e.target.files && setFilename(e.target.files[0])} />
+
+            <Button type="submit" isLoading={isLoading}>Enviar</Button>
         </form>
     )
 }
