914ae2de4189507cf76c17c176bc009bd1fe7d77
samih freire <samihcisne@gmail.com>
2025-10-23 17:58:25 -0300

    feat: criado page de confirmação da solicitalçao de reembolso

--- src/pages/Refund.tsx
+++ src/pages/Refund.tsx
@@ -1,6 +1,8 @@
-import { use, useState } from "react"
+import { useState } from "react"
+import { useNavigate } from "react-router"
 
 import { CATEGORIES, CATEGORIES_KEYS } from "../utils/categories"
+
 import { Input } from "../components/Input"
 import { Select } from "../components/Select"
 import { Upload } from "../components/Upload"
@@ -13,10 +15,15 @@ export function Refund() {
     const [isLoading, setIsLoading] = useState(false);
     const [filename, setFilename] = useState<File | null>(null);
 
+    const navigate = useNavigate();
+
     function onSubmit(e: React.FormEvent) {
         e.preventDefault();
 
         console.log(name, amount, category, filename);
+
+        // Criamos um state para sinalizar para a navegação que ela esta indo por um submit
+        navigate("/confirm", { state: { fromSubmit: true } });
     }
 
     return (
